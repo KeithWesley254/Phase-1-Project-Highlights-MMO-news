e19959d0a6ef7e769de56f69947bcdcbb2d04d46
Keith <keithwesley187@gmail.com>
2022-06-21 02:05:34 +0300

    Video Player and list

---
+++ src/script.js
@@ -0,0 +1,17 @@
+const myListedVideos = document.querySelectorAll('.video-list .vid')
+const mainVideo = document.querySelector('.main-video video')
+const title = document.querySelectorAll('.main-video .video-title')
+
+//Video Player Section
+myListedVideos.forEach(video => {
+    video.onclick = () => {
+        myListedVideos.forEach(vid => vid.classList.remove('active'))
+        video.classList.add('active')
+        if(video.classList === 'active'){
+            let src = video.children[0].getAttribute('src');
+            mainVideo.src = src;
+            let text = video.children[1].innerHTML;
+            title.innerHTML = text
+        }
+    }
+})
